3cc732ec5397dea8cf79fd92d8198d721aabc11d
homin lee <homin.lee@oculus.com>
2017-04-19 17:58:09 -0700

    removing full codes, but will add libraries and header later for devs using in-house engine

--- CaptureNativeSDK/Wamedia/include/whatsappSpecificDefinitions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// 'beam' stands for "Brian-Ehren-Abhishek-Milan", atendees of the initial versioning meeting
-#define WA_VIDEOPREVIEW_SPECIFIC_MP4_BOX_TYPE (CHARS_TO_INT32('v','p','r','w'))
-#define WA_ANIMGIF_SPECIFIC_MP4_BOX_TYPE      (CHARS_TO_INT32('l','o','o','p'))
-
-
