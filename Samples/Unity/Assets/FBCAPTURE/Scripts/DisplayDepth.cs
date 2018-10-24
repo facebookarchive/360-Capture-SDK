0248ba685c6822f223537a0cc1a75d47d2784e48
grancia <homin.lee@oculus.com>
2018-10-24 15:05:57 -0700

    Update Unity sample

--- Samples/Unity/Assets/FBCAPTURE/Scripts/DisplayDepth.cs
+++ Samples/Unity/Assets/FBCAPTURE/Scripts/DisplayDepth.cs
@@ -1,4 +1,14 @@
-﻿using System.Collections;
+﻿/*
+*  Copyright (c) 2014, Facebook, Inc.
+*  All rights reserved.
+*
+*  This source code is licensed under the BSD-style license found in the
+*  LICENSE file in the root directory of this source tree. An additional grant
+*  of patent rights can be found in the PATENTS file in the same directory.
+*
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +20,8 @@ namespace FBCapture {
 
 		private void Start() {
 			depthCamera = GetComponent<Camera>();
-			depthCamera.depthTextureMode = DepthTextureMode.Depth;
+      depthCamera.enabled = false;
+      depthCamera.depthTextureMode = DepthTextureMode.Depth;
 			depthMaterial = CreateMaterial(depthShader, depthMaterial);
 		}
 
---
+++ Samples/Unity/Assets/SpinningCube.cs
@@ -0,0 +1,21 @@
+﻿/*
+*  Copyright (c) 2014, Facebook, Inc.
+*  All rights reserved.
+*
+*  This source code is licensed under the BSD-style license found in the
+*  LICENSE file in the root directory of this source tree. An additional grant
+*  of patent rights can be found in the PATENTS file in the same directory.
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningCube : MonoBehaviour {
+  private float speed = 100.0f;
+
+  void Update () {
+    transform.Rotate(Vector3.up, speed * Time.deltaTime);
+  }
+}
